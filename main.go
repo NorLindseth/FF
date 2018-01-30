b016805e1a91f3e145df2974b2409f5b27d7893d
Yngve <Lindseth94@outlook.com>
2018-01-30 17:34:13 +0100

    Golang now able to parse sql & nullvalues Before SQL session, removing the prisbeskrivelse n:m relationship

--- main.go
+++ main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"log"
+	"net/http"
+	"reflect"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-  db, err := sql.Open("mysql",
-		"root:@tcp(127.0.0.1:3306)/hello")
+	db, err := sql.Open("mysql",
+		"root:@tcp(localhost:3306)/produkt_db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-  err = db.Ping()
-  if err != nil {
-  	log.Println("ERROR SQL")
-  }
+	err = db.Ping()
+	if err != nil {
+		log.Println("ERROR SQL", err)
+	}
 
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/", fs)
+	rows, err := db.Query("SELECT * FROM primærkategori, subkategori WHERE primærkategori.id = subkategori.primær_id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var id int
+		var navn string
+		var id2 int
+		var navn2 string
+		var beskrivelse sql.NullString
+		//var beskrivelse2 string
+		var primær_id int
 
-  log.Println("Listening...")
-  http.ListenAndServe(":80", nil)
+		err = rows.Scan(&id, &navn, &id2, &navn2, &beskrivelse, &primær_id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if reflect.TypeOf(beskrivelse) == nil {
+			//beskrivelse2 = "N/A"
+		}
+		//beskrivelse2 = "test"
+
+		log.Println(id)
+		log.Println(navn)
+		log.Println(id2)
+		log.Println(navn2)
+		log.Println(beskrivelse)
+		log.Println(primær_id)
+	}
+	log.Println("Listening...")
+	http.ListenAndServe(":80", nil)
 }
