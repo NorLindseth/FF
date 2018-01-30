c0445886ac83b7a8db4ad75f7824741b9430e174
Yngve <Lindseth94@outlook.com>
2018-01-30 20:03:20 +0100

    sql for inventory/prices done

--- SQL/DummyData.sql
+++ SQL/DummyData.sql
@@ -35,32 +35,39 @@ INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'uten skinn',
         1
 );
-INSERT INTO subkategori (navn, primær_id) VALUES (
+INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'Eldhus røket/speket',
+        'Det beste av røket og speket mat!',
         1
 );
-INSERT INTO subkategori (navn, primær_id) VALUES (
+INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'Ferske koke stykker',
+        'Det beste av kokt mat!',
         1
 );
-INSERT INTO subkategori (navn, primær_id) VALUES (
+INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'Gullfisker',
+        'Det beste av kokt mat!',
         1
 );
-INSERT INTO subkategori (navn, primær_id) VALUES (
+INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'Bambi',
+        'Det beste av kokt mat!',
         2
 );
-INSERT INTO subkategori (navn, primær_id) VALUES (
+INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'Lam',
+        'Det beste av kokt mat!',
         2
 );
-INSERT INTO subkategori (navn, primær_id) VALUES (
+INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'Hjort, elg, reinsdyr og rådyr',
+        'Det beste av kokt mat!',
         2
 );
-INSERT INTO subkategori (navn, primær_id) VALUES (
+INSERT INTO subkategori (navn, beskrivelse, primær_id) VALUES (
         'Pølser!',
+        'Det beste av kokt mat!',
         2
 );
 
@@ -72,7 +79,7 @@ INSERT INTO produkter (navn, beskrivelse, annen_beskrivelse, bilde_url, antall_p
         'Superior kvalitet',
         'URL DASDASD',
         30,
-        167,
+        10000,
         2,
         2
 );
@@ -83,7 +90,7 @@ INSERT INTO produkter (navn, beskrivelse, annen_beskrivelse, bilde_url, antall_p
         'Superior kvalitet',
         'URL DASasdDASD',
         30,
-        1672,
+        16.725,
         2,
         1
 );
@@ -97,4 +104,64 @@ INSERT INTO produkt_allergen (produkt_id, allergen_id) VALUES (
 INSERT INTO produkt_allergen (produkt_id, allergen_id) VALUES (
     1,
     2
+);
+
+INSERT INTO type (beskrivelse) VALUES (
+        "n for m"
+);
+INSERT INTO type (beskrivelse) VALUES (
+        "n % rabatt, min p kr i rabatt, max q kr i rabatt, eksempel 10% rabatt gitt at det blir gitt mer en 100 kr avslag med maksimum 1000kr avslag, dvs 10% av på handlekurv med pris mellom 1000-10000 kr."
+);
+INSERT INTO tilbud (beskrivelse, rabatt, min, max, konflikt, aktiveringsdato, utløpsdato, type_id) VALUES (
+        "3 for 2 på alle pølser!",
+        2,
+        3,
+        NULL,
+        1,
+        '2008-7-04',
+        '2088-7-04',
+        1
+);
+INSERT INTO tilbud (beskrivelse, rabatt, min, max, konflikt, aktiveringsdato, utløpsdato, type_id) VALUES (
+        "32 for 23 på alle pølser!",
+        2,
+        3,
+        NULL,
+        1,
+        '2008-7-04',
+        '2088-7-04',
+        1
+);
+INSERT INTO tilbud (beskrivelse, rabatt, min, max, konflikt, aktiveringsdato, utløpsdato, type_id, kampanjekode) VALUES (
+        "2% av på alle varer",
+        2,
+        3,
+        NULL,
+        1,
+        '2008-7-04',
+        '2088-7-04',
+        1,
+        'HØST2018'
+);
+INSERT INTO tilbud (beskrivelse, rabatt, min, max, konflikt, aktiveringsdato, utløpsdato, type_id, kampanjekode) VALUES (
+        "20% avslag på kjøp over 3000, til 10000",
+        0.2,
+        600,
+        2000,
+        2,
+        '2008-7-04',
+        '2088-7-04',
+        1,
+        NULL
+);
+INSERT INTO tilbud (beskrivelse, rabatt, min, max, konflikt, aktiveringsdato, utløpsdato, type_id, kampanjekode) VALUES (
+        "202% savslag på kjøp over 3000, til 10000",
+        0.2,
+        600,
+        2000,
+        0,
+        '2008-7-04',
+        '2088-7-04',
+        1,
+        NULL
 );
--- SQL/initTables.sql
+++ SQL/initTables.sql
@@ -1,17 +1,19 @@
 DROP TABLE IF EXISTS
-    produkt_prisbeskrivelse;
-DROP TABLE IF EXISTS
     produkt_allergen;
 DROP TABLE IF EXISTS
     allergen;
 DROP TABLE IF EXISTS
     produkter;
 DROP TABLE IF EXISTS
-        prisbeskrivelse;
+    prisbeskrivelse;
 DROP TABLE IF EXISTS
     subkategori;
 DROP TABLE IF EXISTS
     primærkategori;
+DROP TABLE IF EXISTS
+    tilbud;
+DROP TABLE IF EXISTS
+    type;
 CREATE TABLE IF NOT EXISTS primærkategori (
     id TINYINT AUTO_INCREMENT PRIMARY KEY,
     navn VARCHAR(30) NOT NULL UNIQUE
@@ -19,7 +21,7 @@ CREATE TABLE IF NOT EXISTS primærkategori (
 CREATE TABLE IF NOT EXISTS subkategori (
     id TINYINT AUTO_INCREMENT PRIMARY KEY,
     navn VARCHAR(30) NOT NULL UNIQUE,
-    beskrivelse VARCHAR(30),
+    beskrivelse VARCHAR(250) NOT NULL,
     primær_id TINYINT NOT NULL,
     FOREIGN KEY(primær_id) REFERENCES primærkategori(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = innodb DEFAULT CHARSET = utf8;
@@ -34,7 +36,7 @@ CREATE TABLE IF NOT EXISTS produkter (
     annen_beskrivelse VARCHAR(30),
     bilde_url VARCHAR(30) NOT NULL,
     antall_på_lager SMALLINT,
-    ordinærpris SMALLINT NOT NULL,
+    ordinærpris DECIMAL(8,2) NOT NULL,
     subkategori_id TINYINT NOT NULL,
     prisbeskrivelse_id TINYINT NOT NULL,
     FOREIGN KEY(prisbeskrivelse_id) REFERENCES prisbeskrivelse(id) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -44,10 +46,27 @@ CREATE TABLE IF NOT EXISTS allergen (
     id TINYINT AUTO_INCREMENT PRIMARY KEY,
     beskrivelse VARCHAR(30) NOT NULL UNIQUE
 ) ENGINE = innodb DEFAULT CHARSET = utf8;
-CREATE TABLE produkt_allergen (
+CREATE TABLE IF NOT EXISTS produkt_allergen (
     produkt_id SMALLINT NOT NULL,
     FOREIGN KEY(produkt_id) REFERENCES produkter(id) ON DELETE CASCADE ON UPDATE CASCADE,
     allergen_id TINYINT NOT NULL,
     FOREIGN KEY(allergen_id) REFERENCES allergen(id) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY(produkt_id, allergen_id)
+) ENGINE = innodb DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS type (
+    id TINYINT AUTO_INCREMENT PRIMARY KEY,
+    beskrivelse VARCHAR(250) NOT NULL UNIQUE
+) ENGINE = innodb DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS tilbud (
+    id TINYINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    beskrivelse VARCHAR(250) NOT NULL UNIQUE,
+    rabatt DECIMAL(7,2),
+    min DECIMAL(7,2),
+    max DECIMAL(7,2),
+    konflikt BOOLEAN NOT NULL,
+    aktiveringsdato DATETIME NOT NULL,
+    utløpsdato DATETIME NOT NULL,
+    type_id TINYINT NOT NULL,
+    kampanjekode varchar(20) UNIQUE,
+    FOREIGN KEY(type_id) REFERENCES type(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = innodb DEFAULT CHARSET = utf8;
